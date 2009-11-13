9d5d57a1b57c7f825d750ab57ba45e78bcfe761c
Alexey I. Churkin <phoebus@osiacat.ru>
2009-11-13 10:03:26 +0200

    - unused code

--- lib/App/Web/WebContext.class.php
+++ lib/App/Web/WebContext.class.php
@@ -21,53 +21,7 @@
  */
 final class WebContext extends AppContext implements IWebContext
 {
-	/**
-	 * @var WebContext
-	 */
-	static private $current;
-
-	/**
-	 * @return WebContext
-	 */
-	static function getCurrent()
-	{
-		Assert::isNotNull(self::$current);
-
-		return self::$current;
-	}
-
-	/**
-	 * @return void
-	 */
-	static function setCurrent(WebContext $context)
-	{
-		self::$current = $context;
-	}
-
-	function __construct(WebRequest $request, WebResponse $response, IServerState $server)
-	{
-		if (!self::$current) {
-			self::$current = $this;
-		}
-
-		parent::__construct($request, $response, $server);
-	}
-
-	/**
-	 * @return WebRequest
-	 */
-	function getRequest()
-	{
-		return parent::getRequest();
-	}
-
-	/**
-	 * @return WebResponse
-	 */
-	function getResponse()
-	{
-		return parent::getResponse();
-	}
+	// nothing to override
 }
 
 ?>
