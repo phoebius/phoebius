38d934b7bad496c843bac1e3ade7a69861e24037
Alexey I. Churkin <phoebus@osiacat.ru>
2010-03-29 15:20:55 +0300

    * master now 1.2.x, 1.0.x is not supported

--- etc/scripts/kernel.inc.php
+++ etc/scripts/kernel.inc.php
@@ -16,7 +16,7 @@
  *
  ************************************************************************************************/
 
-define('PHOEBIUS_VERSION', '1.0.1');
+define('PHOEBIUS_VERSION', '1.2.0');
 
 $initializeRootDirectory = str_replace(array('/','\\'), DIRECTORY_SEPARATOR, dirname(__FILE__));
 $initializeRootDirectory = explode(DIRECTORY_SEPARATOR, $initializeRootDirectory);
